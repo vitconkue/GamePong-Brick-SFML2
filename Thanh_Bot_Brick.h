36b4c876dc5a2eb57408eec35431ffbe1f0c4c26
AN VÕ THIỆN <voan0935995700@gmail.com>
2019-12-14 13:53:40 +0700

    Sửa menu, thêm comment

--- Thanh_Bot_Brick.h
+++ Thanh_Bot_Brick.h
@@ -1,11 +1,13 @@
-#pragma once
+﻿#pragma once
 #include "ThanhNguoiChoi_Brick.h"
 #include "Ball_BrickGame.h"
+
+// thanh trượt tự chạy, kế thừa từ thanh trượt thường
 class Thanh_Bot_Brick : public ThanhNguoiChoi_Brick
 {
 private :
 public:
-	void CapNhat(RenderWindow* window, Ball_BrickGame* _ball);
-	Thanh_Bot_Brick(); 
+	void CapNhat(RenderWindow* window, Ball_BrickGame* _ball); // cập nhất các yếu tố liên quan
+	Thanh_Bot_Brick(); // constructor
 };
 
